c1a425576885248fde1dcd882bb71c2d76ea69bf
Igor Bonadio <igorbonadio@gmail.com>
2014-12-18 12:10:56 -0200

    Tests if the initialization creates the right folders

--- test/test_initialization.sh
+++ test/test_initialization.sh
@@ -5,6 +5,23 @@ test \
   should_raise 0
 
 test \
+  "make init" \
+  "ls" \
+  should_output "Config.mk\nMakefile\ndep\ndoc\ninclude\nsrc"
+
+test \
   "cp ../resources/hello.cpp hello.cpp" \
   "make standard" \
   should_raise 0
+
+test \
+  "cp ../resources/hello.cpp hello.cpp" \
+  "make standard" \
+  "ls" \
+  should_output "Config.mk\nMakefile\ndep\ndoc\ninclude\nsrc"
+
+test \
+  "cp ../resources/hello.cpp hello.cpp" \
+  "make standard" \
+  "ls src" \
+  should_output "hello.cpp"

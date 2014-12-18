db40cd035159db2da8a2e4061ab9fdc3843e6a0c
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2014-12-18 21:53:16 -0200

    Adds tests for C/C++ static library compilation

---
+++ test/c++/test_static-lib.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Tests if 'make' builds the library
+test \
+  "make init" \
+  "cp ../resources/static_lib.cpp.mk Config.mk" \
+  "cp ../resources/simple_calc.dat ." \
+  "cp ../resources/simple_calc.cpp ." \
+  "cp ../resources/mathlib.cpp ." \
+  "cp ../resources/mathlib.hpp ." \
+  "make standard" \
+  "make" \
+  "./bin/calculator < ./data/simple_calc.dat" \
+  should_output "4\n2\n27\n2\n1"

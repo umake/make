eb2c8e5e347448e3eed558abc28e94e6b133cd89
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2015-01-05 18:56:58 -0200

    Create 'Makeball' for OpenMP API

---
+++ conf/openmp.mk
@@ -0,0 +1,22 @@
+########################################################################
+# conf/openmp.mk                                                       #
+# ================                                                     #
+#                                                                      #
+# Dependencies and compilation requirements for OpenMP.                #
+# -------------------------------------------------------------------- #
+# MAINTEINER_NAME := Renato Cordeiro Ferreira                          #
+# MAINTEINER_MAIL := renato.cferreira@hotmail.com                      #
+# DESCRIPTION     := `OpenMP` (Open Multi-Processing) is an API for    #
+#                    shared memory multiproces programming for C, C++  #
+#                    and Fortran. It consists of compiler directives,  #
+#                    library routines and environment variables that   #
+#                    influence runtime behavior.                       #
+#                    For further info about the API, access:           #
+#                    http://openmp.org/wp/                             #
+########################################################################
+
+# Flags
+# =======
+CFLAGS          += -fopenmp
+CXXFLAGS        += -fopenmp
+LDFLAGS         += -fopenmp

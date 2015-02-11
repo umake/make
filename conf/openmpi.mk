41c6e1601d416ac1d4fc3165f88ac79690ed9de7
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2015-02-11 21:48:19 -0200

    Add a makeball for Open MPI

---
+++ conf/openmpi.mk
@@ -0,0 +1,21 @@
+########################################################################
+# conf/openmpi.mk                                                      #
+# =================                                                    #
+#                                                                      #
+# Dependencies and compilation requirements for Open MPI.              #
+# -------------------------------------------------------------------- #
+# MAINTEINER_NAME := Renato Cordeiro Ferreira                          #
+# MAINTEINER_MAIL := renato.cferreira@hotmail.com                      #
+# DESCRIPTION     := `Open MPI` (Open Message Passing Interface) is an #
+#                    MPI implementation. MPI defines a set of library  #
+#                    routines for building message-passaging programs, #
+#                    in C, C++ and Fortran.                            #
+#                    For further info about this implementation, see:  #
+#                    http://www.open-mpi.org/                          #
+########################################################################
+
+# Compiler
+# ==========
+CC              := mpicc
+FC              := mpicxx
+CXX             := mpifort

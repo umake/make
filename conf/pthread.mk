5f89de6d07ebf386d31b7bc50747978f949c8d49
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2015-03-04 09:21:50 -0300

    Add makeball for POSIX Threads

---
+++ conf/pthread.mk
@@ -0,0 +1,27 @@
+########################################################################
+# conf/pthread.mk                                                      #
+# =================                                                    #
+#                                                                      #
+# Dependencies and compilation requirements for Pthreads.              #
+# -------------------------------------------------------------------- #
+# MAINTEINER_NAME := Renato Cordeiro Ferreira                          #
+# MAINTEINER_MAIL := renato.cferreira@hotmail.com                      #
+# DESCRIPTION     := `Pthreads` (POSIX Threads) is a POSIX standard    #
+#                    for threads. It is implemented as a library with  #
+#                    C programming language types, functions and       #
+#                    constants used to deal with thread management,    #
+#                    mutexes, condition variables and synchronization. #
+#                    For further info about the standard, access:      #
+#                    http://en.wikipedia.org/wiki/POSIX_Threads        #
+########################################################################
+
+# Flags
+# =======
+ASFLAGS         += -pthread
+CFLAGS          += -pthread
+FFLAGS          += -pthread
+CXXFLAGS        += -pthread
+
+# Linker flags
+# ==============
+LDFLAGS         += -pthread

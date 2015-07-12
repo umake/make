604e3b63ce0b9946d32d328a87f8a2cfa086b854
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2015-07-12 12:00:22 -0300

    Execute in verbose mode to debug in Travis

--- test/c/test_single-file.sh
+++ test/c/test_single-file.sh
@@ -9,6 +9,6 @@ test "$MAKE with a single file"\
 test "if \"$MAKE\" builds the project with a single file" \
   "$MAKE init" \
   "cp ../resources/hello.c src/hello.c" \
-  "$MAKE" \
+  "$MAKE V=1" \
   "./bin/hello" \
   should_output "Hello, World!"

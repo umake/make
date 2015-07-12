70249dabf6e0d90bd813c164a1da74857bd25837
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2015-07-12 12:18:57 -0300

    Remove verbose mode

--- test/c/test_single-file.sh
+++ test/c/test_single-file.sh
@@ -3,7 +3,7 @@
 test "$MAKE with a single file"\
   "$MAKE init" \
   "cp ../resources/hello.c src/hello.c" \
-  "$MAKE V=1" \
+  "$MAKE" \
   should_raise 0
 
 test "if \"$MAKE\" builds the project with a single file" \

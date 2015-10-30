5c53163366353eb1193a7fe310fa51268f36ad1e
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2015-10-30 15:54:50 -0200

    Update eigen makeball

--- conf/eigen.mk
+++ conf/eigen.mk
@@ -18,9 +18,11 @@ override EIGEN_MK := T
 # Dependencies
 # ==============
 WEB_DEPENDENCY  += \
-    eigen => https://bitbucket.org/eigen/eigen/get/3.2.5.tar.gz    \
-            extract: mv eigen eigen-3.2.5.tar.gz && tar -xvf eigen-3.2.5.tar.gz && mv eigen-eigen-bdd17ee3b1b3 eigen \
-            build: echo eigen installed
+    eigen => https://bitbucket.org/eigen/eigen/get/3.2.6.tar.gz \
+             extract: mv eigen eigen-3.2.6.tar.gz \
+			          && tar -xvf eigen-3.2.6.tar.gz \
+					  && mv eigen*/ eigen \
+             build:   echo eigen installed
 
 # Paths
 # =======

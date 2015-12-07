8e925d2c7b69555127cc2e613e8463da3f67a43d
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2015-12-07 10:17:10 -0200

    Add ChaiScript makeball

---
+++ conf/chaiscript.mk
@@ -0,0 +1,38 @@
+########################################################################
+# conf/chaiscript.mk                                                   #
+# ====================                                                 #
+#                                                                      #
+# Dependencies and compilation requirements for ChaiScript.            #
+# -------------------------------------------------------------------- #
+# MAINTEINER_NAME := Renato Cordeiro Ferreira                          #
+# MAINTEINER_MAIL := renato.cferreira@hotmail.com                      #
+# DESCRIPTION     := `ChaiScript` is an easy to use embedded scripting #
+#                    language for C++. It can be used as header only,  #
+#                    depending only on a C++11 compliant compiler.     #
+#                    By default, ChaiScript is thread safe (what can   #
+#                    be disabled for enhanced performance) and s fully #
+#                    tested for 32-bit and 64-bit on g++, clang++ and  #
+#                    Windows (MSVC2013).                               #
+#                    For further info about ChaiScript, access:        #
+#                    http://chaiscript.com/                            #
+########################################################################
+
+ifndef CHAISCRIPT_MK
+override CHAISCRIPT_MK := T
+
+# Dependencies
+# ==============
+GIT_DEPENDENCY  += \
+    chaiscript => https://github.com/ChaiScript/ChaiScript \
+                  build: mkdir -p build && cd build && cmake .. && make
+
+# Paths
+# =======
+CXXLIBS         += -I external/chaiscript/include
+LDLIBS          += -L external/chaiscript/build
+
+# Linker flags
+# ==============
+LDCXX           += -ldl -lchaiscript_stdlib-5.7.2
+
+endif # ifndef CHAISCRIPT_MK
